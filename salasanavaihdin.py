25cfcc92ac48c1763331b64d4a1e414963a5dea1
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-03-30 13:50:57 +0300

    testi toimii

--- salasanavaihdin.py
+++ salasanavaihdin.py
@@ -1,12 +1,19 @@
 import random
 import string
+import unittest
+
+runtest = 1
 
 def generate_password():
-    nominit = ['pöytä', 'tuoli', 'omena,', 'päärynä', 'pyörä', 'ruuvi', 'ruusu', 'neilikka', 'leppäkerttu', 'perhonen',
-    'kenkä', 'sukka', 'puhelin', 'kello', 'radio', 'telkkari', 'jauho', 'jyvä', 'kynsi', 'tukka', 'vesi', 'öljy', 'kivi', 'puurunko']
+    nominit = ['pöytä', 'tuoli', 'omena,', 'päärynä', 'pyörä', 'ruuvi', 
+               'ruusu', 'neilikka', 'leppäkerttu', 'perhonen', 'kala', 'mökki',
+               'kenkä', 'sukka', 'puhelin', 'kello', 'radio', 'telkkari', 'jauho', 
+               'jyvä', 'kynsi', 'tukka', 'vesi', 'öljy', 'kivi', 'puurunko']
 
-    adjektiivit = ['nopea', 'hitas', 'kiltti', 'tuhma', 'iso', 'pieni', 'pimeä', 'vaalea', 'tahmea', 'liukas', 'punainen', 'sininen',
-    'hiljas', 'äänekäs', 'jännittävä', 'tylsä', 'kylmä', 'lämmin', 'tuulinen', 'tyyni', 'komea', 'ruma', 'kevyt', 'painava', 'kiero', 'suora']
+    adjektiivit = ['nopea', 'hitas', 'kiltti', 'tuhma', 'iso', 'pieni', 'pimeä',
+                   'vaalea', 'tahmea', 'liukas', 'punainen', 'sininen',
+                   'hiljas', 'äänekäs', 'jännittävä', 'tylsä', 'kylmä', 'lämmin', 'tuulinen', 
+                   'tyyni', 'komea', 'ruma', 'kevyt', 'painava', 'kiero', 'suora']
     
     while True:
         adjektiivi = random.choice(adjektiivit)
@@ -22,4 +29,14 @@ def generate_password():
 uusi_salasana = generate_password()
 print(uusi_salasana)
 
+if runtest == 0:
+    generate_password()
+
 # Testit
+# Class nimi pitää olla sama kuin tiedoston nimi
+class salasanavaihdin(unittest.TestCase):
+    def test_generate_password_success(self):
+        actual = len(generate_password())
+        expected = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+        print('actual= ', actual)
+        self.assertIn(actual, expected)
